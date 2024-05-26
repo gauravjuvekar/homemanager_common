8bb4fd0b483b81bd6f1dca0ec3f9246882f58bd1
Gaurav Juvekar <gjuvekar@nvidia.com>
2024-05-26 10:01:49 -0700

    Add gpg config

--- common/interactive.nix
+++ common/interactive.nix
@@ -1,5 +1,9 @@
-{ config, pkgs, ... }:
+{  pkgs, ... }:
 {
+  imports =
+    [
+      ./gpg/gpg.nix
+    ];
   home.packages = with pkgs;
     [
       (hiPrio parallel) # Because moreutils overwrites it
--- common/interactive_yubikey.nix
+++ common/interactive_yubikey.nix
@@ -1,7 +1,15 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 {
   home.packages = with pkgs;
     [
       yubikey-manager
     ];
+  programs.gpg =
+    {
+      scdaemonSettings =
+        {
+          disable-ccid = true;
+          reader-port = "Yubico Yubi";
+        };
+    };
 }
