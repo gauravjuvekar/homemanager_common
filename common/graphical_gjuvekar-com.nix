f0922ce2da0518328fa97db300d030690aa8a5d6
Gaurav Juvekar <gauravjuvekar@gmail.com>
2024-05-26 21:15:01 -0700

    Add bindfs for gnucash readonly mount

--- common/graphical_gjuvekar-com.nix
+++ common/graphical_gjuvekar-com.nix
@@ -1,13 +1,15 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 {
+  imports =
+    [
+      ./gnucash/gnucash.nix
+    ];
   home.packages = with pkgs;
     [
       calibre
       clementine
-      gnucash
       kid3-qt
       picard
       python312Packages.eyed3
-      syncthing
     ];
 }
