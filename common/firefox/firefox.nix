61227848c9a04d6162b675ea1effdabca42e6936
Gaurav Juvekar <gauravjuvekar@gmail.com>
2024-08-05 03:07:26 -0700

    firefox: uiCustomizationState

--- common/firefox/firefox.nix
+++ common/firefox/firefox.nix
@@ -40,6 +40,81 @@
               "browser.toolbars.bookmarks.showOtherBookmarks" = false;
               "browser.toolbars.bookmarks.visibility" = "always";
               "browser.uidensity" = 1;
+              "browser.uiCustomization.state" =
+                {
+                  "currentVersion" = 20;
+                  "dirtyAreaCache" =
+                    [
+                      "PersonalToolbar"
+                      "TabsToolbar"
+                      "nav-bar"
+                      "toolbar-menubar"
+                      "unified-extensions-area"
+                      "widget-overflow-fixed-list"
+                    ];
+                  "newElementCount" = 10;
+                  "placements" =
+                    {
+                      "nav-bar" =
+                        [
+                          "back-button"
+                          "forward-button"
+                          "downloads-button"
+                          "stop-reload-button"
+                          "urlbar-container"
+                          "search-container"
+                          "fxa-toolbar-menu-button"
+                          "reset-pbm-toolbar-button"
+                          "unified-extensions-button"
+                        ];
+                      "PersonalToolbar" =
+                        [
+                          "sidebar-button"
+                          "_73a6fe31-595d-460b-a920-fcc0f8843232_-browser-action" # noscript
+                          "ublock0_raymondhill_net-browser-action"
+                          "cookieautodelete_kennydo_com-browser-action"
+                          "addon_darkreader_org-browser-action"
+                          "personal-bookmarks"
+                          "offline-qr-code_rugk_github_io-browser-action"
+                          "_a6c4a591-f1b2-4f03-b3ff-767e5bedf4e7_-browser-action" # user agent switcher
+                          "printedit-we_dw-dev-browser-action"
+                          "developer-button"
+                          "zoompage-we_dw-dev-browser-action"
+                          "zoom-controls"
+                          "managed-bookmarks"
+                        ];
+                      "toolbar-menubar" =
+                        [
+                          "menubar-items"
+                        ];
+                      "TabsToolbar" =
+                        [
+                          "tabbrowser-tabs"
+                          "new-tab-button"
+                          "sync-button"
+                          "simple-tab-groups_drive4ik-browser-action"
+                          "treestyletab_piro_sakura_ne_jp-browser-action"
+                          "alltabs-button"
+                        ];
+                      "unified-extensions-area" =
+                        [
+                          "_ddc62400-f22d-4dd3-8b4a-05837de53c2e_-browser-action" # read aloud
+                          "_d7742d87-e61d-4b78-b8a1-b469842139fa_-browser-action" # vimium
+                          "_tst-search-browser-action"
+                          "redirector_einaregilsson_com-browser-action"
+                          "_0d7cafdd-501c-49ca-8ebb-e3341caaa55e_-browser-action" # youtube nonstop
+                          "_e855175b-f84a-429d-85d6-a61831c8291c_-browser-action" # tab retitle
+                          "firemonkey_eros_man-browser-action"
+                          "foxytab_eros_man-browser-action"
+                          "jid0-bnmfwww2w2w4e4edvcddbnmhdvg_jetpack-browser-action" # tab reloader
+                          "jid1-bofifl9vbdl2zq_jetpack-browser-action" # decentraleyes
+                          "jid1-kkzogwgsw3ao4q_jetpack-browser-action" # i don't care about cookies
+                          "_506e023c-7f2b-40a3-8066-bc5deb40aebe_-browser-action" # gesturfy
+                          "_7a7a4a92-a2a0-41d1-9fd7-1e92480d612d_-browser-action" # stylus
+                        ];
+                      "widgets-overflow-fixed-list" = [];
+                    };
+                };
               "browser.urlbar.matchBuckets" = "general:5 =suggestion:Infinity";
               "browser.urlbar.quicksuggest.dataCollection.enabled" = false;
               "browser.urlbar.quicksuggest.nonsponsored" = false;
