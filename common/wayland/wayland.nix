80e808274fbedcd4b3bbcf9dd0b8bebd22067ebb
Gaurav Juvekar <gauravjuvekar@gmail.com>
2024-08-11 15:48:21 -0700

    wayland: add scripts to allow running GUI applications as root

--- common/wayland/wayland.nix
+++ common/wayland/wayland.nix
@@ -1,9 +1,25 @@
 { pkgs, ... }:
+let
+  wayland_gui_allow_root = pkgs.writeShellScriptBin "wayland-gui-allow-root"
+    ''
+      ${pkgs.xorg.xhost}/bin/xhost si:localuser:root
+    '';
+  wayland_gui_forbid_root = pkgs.writeShellScriptBin "wayland-gui-forbid-root"
+    ''
+      ${pkgs.xorg.xhost}/bin/xhost -si:localuser:root
+    '';
+in
 {
-  home.packages = with pkgs;
+  home.packages =
+    (
+      with pkgs; [
+          shikane
+          wdisplays
+        ]
+    ) ++
     [
-      shikane
-      wdisplays
+      wayland_gui_allow_root
+      wayland_gui_forbid_root
     ];
 }
 
