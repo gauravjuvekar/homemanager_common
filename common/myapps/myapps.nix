8f226960887f2fe8812e067f109e1b1ceaa6186f
Gaurav Juvekar <gauravjuvekar@gmail.com>
2024-08-18 18:13:12 -0700

    shell: add g-clean-untracked

--- common/myapps/myapps.nix
+++ common/myapps/myapps.nix
@@ -5,6 +5,19 @@
       (
         pkgs.writeShellApplication
           {
+            name = "g-clean-untracked";
+            runtimeInputs =
+              [
+                pkgs.git
+                pkgs.parallel
+                pkgs.ripgrep
+              ];
+            text = (builtins.readFile ./g-clean-untracked);
+          }
+      )
+      (
+        pkgs.writeShellApplication
+          {
             name = "mv_versioned";
             text = (builtins.readFile ./mv_versioned);
           }
