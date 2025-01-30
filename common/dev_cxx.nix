83c9abb65c1976d40e00527d8db5e4cd4469cbfa
Gaurav Juvekar <gauravjuvekar@gmail.com>
2025-01-30 00:05:38 -0800

    compiledb: separate package

--- common/dev_cxx.nix
+++ common/dev_cxx.nix
@@ -5,8 +5,8 @@
       (hiPrio gcc)  # HOLY SHIT, Clang is broken GH 277564, 153759, 147342
       clang
       clang-tools
+      compiledb
       llvmPackages.bintools-unwrapped
       llvmPackages.libcxxStdenv
-      python311Packages.compiledb
     ];
 }
