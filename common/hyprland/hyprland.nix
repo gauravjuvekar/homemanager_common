9816af907500b674143b29a22d9ec80e140af28d
Gaurav Juvekar <gauravjuvekar@gmail.com>
2025-03-16 16:41:49 -0700

    hyprland: basic waybar

--- common/hyprland/hyprland.nix
+++ common/hyprland/hyprland.nix
@@ -222,5 +222,102 @@
           cmd="${pkgs.swaynotificationcenter}/bin/swaync";
         };
     };
+
+  programs.waybar =
+    {
+      enable = true;
+      systemd = { enable = true; };
+      settings =
+        {
+          mainBar =
+            {
+              layer = "top";
+              position = "top";
+              height = 32;
+              modules-left =
+                [
+                  "hyprland/workspaces"
+                  "wlr/taskbar"
+                ];
+              modules-center =
+                [
+                  "hyprland/window"
+                ];
+              modules-right =
+                [
+                  "keyboard-state"
+                  "hyprland/submap"
+                  "bluetooth"
+                  "pulseaudio"
+                  "clock"
+                  "idle_inhibitor"
+                  "systemd-failed-units"
+                ];
+
+              "hyprland/workspaces" =
+                {
+                  disable-scroll = true;
+                  all-outputs = true;
+                };
+
+              keyboard-state =
+                {
+                  numlock = true;
+                  capslock = true;
+                  scrolllock = true;
+                };
+
+              "wlr/taskbar" =
+                {
+                  on-click = "activate";
+                  on-click-middle = "close";
+
+                };
+
+
+              idle_inhibitor =
+                {
+                  format = "{icon}";
+                  format-icons =
+                    {
+                      activated = builtins.fromJSON '' "\uf0f4" ''; # icon-coffee
+                      deactivated = builtins.fromJSON '' "\uf186" ''; # icon-moon
+
+                    };
+                };
+
+              clock =
+                {
+                  interval = 1;
+                  format = "{:%a %F %T%z %Z}";
+                  tooltip-format =
+                    "<tt>{tz_list}</tt>" +
+                    "\n" +
+                    "<tt>{calendar}</tt>";
+                  timezones =
+                    [
+                      "America/Los_Angeles"
+                      "Asia/Kolkata"
+                      "America/New_York"
+                      "Asia/Taipei"
+                      "Etc/UTC"
+                    ];
+                  actions =
+                    {
+                      on-click-right = "mode";
+                      on-scroll-up = "tz_up";
+                      on-scroll-down = "tz_down";
+                    };
+                  calendar =
+                    {
+                      mode = "year";
+                      mode-mon-col = 3;
+                      week-pos = "left";
+                      on-scroll = 1;
+                    };
+                };
+            };
+        };
+    };
 }
 
