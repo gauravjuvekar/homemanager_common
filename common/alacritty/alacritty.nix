4b7934b090b5b709da57b5037ab32a56dbee31cb
Gaurav Juvekar <gauravjuvekar@gmail.com>
2025-03-26 08:54:18 -0700

    alacritty: set theme

--- common/alacritty/alacritty.nix
+++ common/alacritty/alacritty.nix
@@ -1,10 +1,15 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.alacritty =
     {
       enable = true;
       settings =
         {
+          general.import =
+            [
+              # "${pkgs.alacritty-theme}/hyper.toml" // For OLED: TODO specializations
+              "${pkgs.alacritty-theme}/alabaster_dark.toml"
+            ];
           scrolling =
             {
               history = 100000;
