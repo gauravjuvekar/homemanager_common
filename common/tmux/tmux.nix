414b98c4f17815aab71e5f4c517ae9f9f5dd7979
Gaurav Juvekar <gauravjuvekar@gmail.com>
2025-03-28 16:14:34 -0700

    tmux: fix nvim :checkhealth issues

--- common/tmux/tmux.nix
+++ common/tmux/tmux.nix
@@ -4,13 +4,16 @@
     {
       enable = true;
       clock24 = true;
+      escapeTime =  10;
       extraConfig =
         ''
           bind-key -T copy-mode-vi 'v' send -X begin-selection
           bind-key -T copy-mode-vi 'y' send -X copy-selection-and-cancel
         '';
+      focusEvents = true;
       historyLimit = 10000;
       keyMode = "vi";
       mouse = true;
+      terminal = "screen-256color";
     };
 }
