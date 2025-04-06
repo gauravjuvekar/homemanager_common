30ae57e245e63a516eb9ae45e8db684abadaf79b
Gaurav Juvekar <gauravjuvekar@gmail.com>
2025-04-06 12:20:08 -0700

    gnucash: add readonly mount

--- common/gnucash/gnucash.nix
+++ common/gnucash/gnucash.nix
@@ -1,10 +1,32 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 {
   home.packages = with pkgs;
     [
       bindfs
       gnucash
     ];
+
+  systemd.user.mounts =
+    {
+      "home-${config.home.username}-mnt-gnucash_readonly" =
+        {
+          Unit =
+            {
+              Description = "Read only mount of gnucash for libreoffice base odbc connector";
+            };
+          Mount =
+            {
+              What = "%h/Documents/finances/gnucash";
+              Where = "/home/${config.home.username}/mnt/gnucash_readonly";
+              Type = "fuse.bindfs";
+              Options = "ro,x-gvfs-hide";
+            };
+          Install =
+            {
+              WantedBy = [ "default.target" ];
+            };
+        };
+    };
 
   dconf.settings =
     {
