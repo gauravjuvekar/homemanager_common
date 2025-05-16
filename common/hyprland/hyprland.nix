23889d3b49216d6e2910c0b558780b150470b68e
Gaurav Juvekar <gauravjuvekar@gmail.com>
2025-05-15 22:25:14 -0700

    hypridle: configure

--- common/hyprland/hyprland.nix
+++ common/hyprland/hyprland.nix
@@ -101,6 +101,37 @@
         ];
     };
 
+  services.hypridle =
+    {
+      enable = true;
+      settings =
+        {
+          general =
+            {
+              after_sleep_cmd = "hyprctl dispatch dpms on";
+              before_sleep_cmd = "loginctl lock-session";
+              lock_cmd = "pidof hyprlock || ${config.programs.hyprlock.package}/bin/hyprlock";
+            };
+          listener =
+            [
+              {
+                timeout = 100;
+                on-timeout = "${pkgs.brightnessctl}/bin/brightnessctl -s set 10";
+                on-resume = "${pkgs.brightnessctl}/bin/brightnessctl -r";
+              }
+              {
+                timeout = 300;
+                on-timeout = "${config.programs.hyprlock.package}/bin/hyprlock";
+              }
+              {
+                timeout = 500;
+                on-timeout = "hyprctl dispatch dpms off";
+                on-resume = "hyprctl dispatch dpms on";
+              }
+            ];
+        };
+    };
+
   wayland.windowManager.hyprland =
     {
       enable = true;
