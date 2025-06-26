b5a5dec6f27d41eb2cfb847daa02f97c460a5494
Gaurav Juvekar <gauravjuvekar@gmail.com>
2025-06-26 15:56:42 -0700

    niri: configure portals

--- common/wayland/niri/niri.nix
+++ common/wayland/niri/niri.nix
@@ -234,5 +234,15 @@ binds {
     Mod+Shift+P { power-off-monitors; }
 }
   '';
+
+  xdg.configFile."xdg-desktop-portal/niri-portals.conf".text =
+    ''
+[preferred]
+default=gnome;gtk;
+org.freedesktop.impl.portal.Access=gtk;
+org.freedesktop.impl.portal.FileChooser=gtk;
+org.freedesktop.impl.portal.Notification=gtk;
+org.freedesktop.impl.portal.Secret=gnome-keyring;
+    '';
 }
 
