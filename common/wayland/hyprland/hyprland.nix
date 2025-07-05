e730d3b001c0cb7a6f0c2472b3564f5c0b7b3438
Gaurav Juvekar <gauravjuvekar@gmail.com>
2025-07-05 14:25:32 -0700

    niri: use commands in hyprland.nix for hyprlock and wlogout

--- common/wayland/hyprland/hyprland.nix
+++ common/wayland/hyprland/hyprland.nix
@@ -76,7 +76,8 @@
           }
           {
             "label" = "logout";
-            "action" = "${config.wayland.windowManager.hyprland.finalPackage}/bin/hyprctl dispatch exit";
+            "action" =
+              "${config.wayland.windowManager.hyprland.finalPackage}/bin/hyprctl dispatch exit || ${pkgs.niri}/bin/niri msg action quit";
             "text" = "Logout (e)";
             "keybind" = "e";
           }
@@ -108,7 +109,7 @@
         {
           general =
             {
-              after_sleep_cmd = "hyprctl dispatch dpms on";
+              after_sleep_cmd = "hyprctl dispatch dpms on || ${pkgs.niri}/bin/niri msg action power-on-monitors";
               before_sleep_cmd = "loginctl lock-session";
               lock_cmd = "pidof hyprlock || ${config.programs.hyprlock.package}/bin/hyprlock";
             };
@@ -125,8 +126,8 @@
               }
               {
                 timeout = 500;
-                on-timeout = "hyprctl dispatch dpms off";
-                on-resume = "hyprctl dispatch dpms on";
+                on-timeout = "hyprctl dispatch dpms off || ${pkgs.niri}/bin/niri msg action power-off-monitors";
+                on-resume = "hyprctl dispatch dpms on || ${pkgs.niri}/bin/niri msg action power-on-monitors";
               }
             ];
         };
